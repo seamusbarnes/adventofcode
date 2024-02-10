9084b789fa39cc86f0b45ee4418c2c57f0fa4dc1
seamusbarnes <139384226+seamusbarnes@users.noreply.github.com>
2024-02-10 10:54:38 +0000

    solution.py: put line parse logic in function parse_line

--- Day01/solution.py
+++ Day01/solution.py
@@ -1,29 +1,33 @@
-filename = "Day01_input.txt"
-# filename = 'test_input.txt'
+def main():
+    filename = "Day01_input.txt"
+    # filename = 'test_input.txt'
 
-with open(filename, 'r') as file:
-    lines = [line.strip() for line in file]
+    with open(filename, 'r') as file:
+        lines = [line.strip() for line in file]
 
-def isNum(c):
-    if ord('0') <= ord(c) <= ord('9'):
-        return True
-    return False
+    def isNum(c):
+        if ord('0') <= ord(c) <= ord('9'):
+            return True
+        return False
 
-nums = []
-for line in lines:
-    first, last = None, None
-    for c in line:
-        if isNum(c):
-            if first == None:
-                first = int(c)
-            else:
-                last = int(c)
-    if last == None:
-        last = first
+    def parse_line(line):
+        first, last = None, None
+        for c in line:
+            if isNum(c):
+                if first == None:
+                    first = int(c)
+                else:
+                    last = int(c)
+            if last == None:
+                last = first
+        return (first, last)
+    
+    nums = []
+    for line in lines:
+        first, last = parse_line(line)
+        nums.append((first*10)+last)
+    
+    print(sum(nums))
 
-    num = (first*10)+last
-    nums.append(num)
-
-res = sum(nums)
-
-print(res)
+if __name__ == '__main__':
+    main()
